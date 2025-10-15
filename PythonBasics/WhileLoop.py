f530155221edf56193717522c7bbc335bfc3e949
NourojAmin <99739843+NourojAmin@users.noreply.github.com>
2025-10-15 22:40:15 +0600

    Python Basics

--- PythonBasics/WhileLoop.py
+++ PythonBasics/WhileLoop.py
@@ -2,9 +2,11 @@ age = 1
 
 while age<10:
     if age == 3:
+        print("Continue Executed")
         age = age + 1
         continue
     if age == 9:
+        print("Break Executed")
         break
     print(age)
     age = age+1
